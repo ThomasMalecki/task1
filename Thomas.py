7b2178a899f5a2e529430c259b9d35f7e6d65ab8
11aye <91118264+ThomasMalecki@users.noreply.github.com>
2023-10-05 21:39:29 +0200

    Update Thomas.py

--- Thomas.py
+++ Thomas.py
@@ -1,80 +1,88 @@
+import streamlit as st
 from simpleai.search import CspProblem, backtrack
-words = []
-word1 = input("Geef het eerste woord: ").upper()
-words.append(word1)
 
-word2 = input("Geef het tweede woord: ").upper()
-words.append(word2)
+# Function to solve the CSP problem
+def solve_csp(word1, word2, word3):
+    words = [word1, word2, word3]
 
-word3 = input("Geef het derde woord: ").upper()
-words.append(word3)
+    # Create a list of unique characters
+    variables = list(set(word1 + word2 + word3))
 
-# Create a list of unique characters
-variables = list(set(word1 + word2 + word3))
+    # Remove any non-alphabetical characters
+    variables = [char for char in variables if char.isalpha()]
 
-# Remove any non-alphabetical characters
-variables = [char for char in variables if char.isalpha()]
+    domains = {}
+    # Make a domains object that defines the ranges of the characters; first letters of words cannot have 0
+    for word in words:
+        for i, char in enumerate(word):
+            if i == 0:
+                domains[char] = list(range(1, 10))
+            else:
+                domains[char] = list(range(10))
 
-domains = {}
-# Make a domains object that defines the ranges of the chars first letters of words cannot have 0
-for word in words:
-    for i, char in enumerate(word):
-        print(word[i])
-        if i == 0:
-            domains[char] = list(range(1, 10))
-            print(i)
+    def constraint_unique(variables, values):
+        return len(values) == len(set(values))
+
+    def constraint_add(variables, values):
+        factor1 = ""
+        factor2 = ""
+        result = ""
+        for char in words[0]:
+            position = variables.index(char)
+            factor1 += str(values[position])
+        factor1 = int(factor1)
+        for char in words[1]:
+            position = variables.index(char)
+            factor2 += str(values[position])
+        factor2 = int(factor2)
+        for char in words[2]:
+            position = variables.index(char)
+            result += str(values[position])
+        result = int(result)
+        return (factor1 + factor2) == result
+
+    constraints = [
+        (variables, constraint_unique),
+        (variables, constraint_add),
+    ]
+
+    # Create the CSP problem
+    problem = CspProblem(variables, domains, constraints)
+
+    # Solve the CSP problem
+    output = backtrack(problem)
+
+    if output:
+        solution = {var: output[var] for var in variables}
+        return solution
+    else:
+        return None
+
+# Streamlit UI
+st.title("CSP Solver")
+st.write("Enter three words to find a valid digit assignment:")
+
+word1 = st.text_input("Enter the first word:")
+word2 = st.text_input("Enter the second word:")
+word3 = st.text_input("Enter the third word:")
+
+if st.button("Solve"):
+    if word1 and word2 and word3:
+        word1 = word1.upper()
+        word2 = word2.upper()
+        word3 = word3.upper()
+        
+        solution = solve_csp(word1, word2, word3)
+        
+        if solution:
+            st.write("Solution Found:")
+            st.write("Digits Solution:")
+            for word in [word1, word2, word3]:
+                for char in word:
+                    if char.isalpha():
+                        st.write(solution[char], end=" ")
+                    else:
+                        st.write(char, end=" ")
+                st.write("\n")
         else:
-            domains[char] = list(range(10))
-
-def constraint_unique(variables, values):
-    return len(values) == len(set(values))
-
-def constraint_add(variables, values):
-    factor1 = ""
-    factor2 = ""
-    result = ""
-    for char in words[0]:
-        position = variables.index(char)
-        factor1 += str(values[position])
-    factor1 = int(factor1)
-    for char in words[1]:
-        position = variables.index(char)
-        factor2 += str(values[position])
-    factor2 = int(factor2)
-    for char in words[2]:
-        position = variables.index(char)
-        result += str(values[position])
-    result = int(result)
-    return (factor1 + factor2) == result
-#
-constraints = [
-    (variables, constraint_unique),
-    (variables, constraint_add),
-]
-#here we use simpleAI to backtrack of a solution
-problem = CspProblem(variables, domains, constraints)
-
-output = backtrack(problem)
-
-if output:
-    solution = {var: output[var] for var in variables}
-    
-    print("\nDigits Solution:")
-    for word in words:
-        for char in word:
-            if char.isalpha():
-                print(solution[char], end=" ")
-            else:
-                print(char, end=" ")
-        print()
-
-    print("\nLetters Solution:")
-    for word in words:
-        for char in word:
-            if char.isalpha():
-                print(char, end=" ")
-            else:
-                print(char, end=" ")
-        print()
-else:
-    print("No solution found.")
+            st.write("No solution found.")
