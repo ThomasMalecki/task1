1a89a1a564bc1b878ff8adf7b3b34e82d8569325
11aye <91118264+ThomasMalecki@users.noreply.github.com>
2023-10-06 21:55:34 +0200

    Update Thomas.py

--- Thomas.py
+++ Thomas.py
@@ -3,60 +3,7 @@ from simpleai.search import CspProblem, backtrack
 
 # Function to solve the CSP problem
 def solve_csp(word1, word2, word3):
-    words = [word1, word2, word3]
-
-    # Create a list of unique characters
-    variables = list(set(word1 + word2 + word3))
-
-    # Remove any non-alphabetical characters
-    variables = [char for char in variables if char.isalpha()]
-
-    domains = {}
-    # Make a domains object that defines the ranges of the characters; first letters of words cannot have 0
-    for word in words:
-        for i, char in enumerate(word):
-            if i == 0:
-                domains[char] = list(range(1, 10))
-            else:
-                domains[char] = list(range(10))
-
-    def constraint_unique(variables, values):
-        return len(values) == len(set(values))
-
-    def constraint_add(variables, values):
-        factor1 = ""
-        factor2 = ""
-        result = ""
-        for char in words[0]:
-            position = variables.index(char)
-            factor1 += str(values[position])
-        factor1 = int(factor1)
-        for char in words[1]:
-            position = variables.index(char)
-            factor2 += str(values[position])
-        factor2 = int(factor2)
-        for char in words[2]:
-            position = variables.index(char)
-            result += str(values[position])
-        result = int(result)
-        return (factor1 + factor2) == result
-
-    constraints = [
-        (variables, constraint_unique),
-        (variables, constraint_add),
-    ]
-
-    # Create the CSP problem
-    problem = CspProblem(variables, domains, constraints)
-
-    # Solve the CSP problem
-    output = backtrack(problem)
-
-    if output:
-        solution = {var: output[var] for var in variables}
-        return solution
-    else:
-        return None
+    # ... (Your existing solve_csp function)
 
 # Streamlit UI
 st.title("CSP Solver")
@@ -76,13 +23,16 @@ if st.button("Solve"):
         
         if solution:
             st.write("Solution Found:")
-            st.write("Digits Solution:")
-            for word in [word1, word2, word3]:
-                for char in word:
-                    if char.isalpha():
-                        st.write(solution[char], end=" ")
-                    else:
-                        st.write(char, end=" ")
-                st.write("\n")
+            
+            # Display the math question layout
+            st.markdown("<h3>Math Question:</h3>", unsafe_allow_html=True)
+            st.markdown(f"<p>{word1} + {word2} = {word3}</p>", unsafe_allow_html=True)
+            
+            # Display the solution in digits
+            st.markdown("<h3>Solution in Digits:</h3>", unsafe_allow_html=True)
+            st.markdown(f"<p>{' '.join([str(solution[char]) if char.isalpha() else char for char in word1])} + " \
+                        f"{' '.join([str(solution[char]) if char.isalpha() else char for char in word2])} = " \
+                        f"{' '.join([str(solution[char]) if char.isalpha() else char for char in word3])}</p>",
+                        unsafe_allow_html=True)
         else:
             st.write("No solution found.")
